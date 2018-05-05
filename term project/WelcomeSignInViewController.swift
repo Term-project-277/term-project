762edb2f811b6e2b648e92837945edb9d27eb751
thrishna <thrishna.palissery@gmail.com>
2018-05-05 11:56:02 -0700

    Created Admin Login and Welcome Page

---
+++ term project/WelcomeSignInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeSignInViewController.swift
+//  term project
+//
+//  Created by Nod on 5/5/18.
+//  Copyright © 2018 Shweta Kothari. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
