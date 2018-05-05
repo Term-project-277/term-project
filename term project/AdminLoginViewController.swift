28e33aed92f9abd564f143a720fdc80c30ace98a
thrishna <thrishna.palissery@gmail.com>
2018-05-05 12:25:01 -0700

    Admin password and Login verified again hardcoded values and give permission for admin to login

--- term project/AdminLoginViewController.swift
+++ term project/AdminLoginViewController.swift
@@ -9,19 +9,34 @@
 import UIKit
 
 class AdminLoginViewController: UIViewController {
+    let adminUserName = "Thrishna"
+    let adminPassword = "cmpe@277"
+    var userName = ""
+    var pword = ""
 
+    @IBOutlet weak var username: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var password: UITextField!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
+    @IBAction func login_button(_ sender: Any) {
+        userName = username.text!
+        pword = password.text!
+        if(userName == adminUserName && pword == adminPassword){
+        self.performSegue(withIdentifier: "adminLoginIdentifier", sender: self)
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
