4b50a391c684457638faad7a5ff2039187880f61
thrishna <thrishna.palissery@gmail.com>
2018-05-05 17:36:53 -0700

    Background picture added to welcome page

--- term project/WelcomeSignInViewController.swift
+++ term project/WelcomeSignInViewController.swift
@@ -13,10 +13,26 @@ class WelcomeSignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*let textAttributes = [NSAttributedStringKey.foregroundColor:UIColor.darkGray,
+             NSAttributedStringKey.font: UIFont(name: "Gill Sans", size: 24)!]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes*/
+       
+        
 
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    } 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
