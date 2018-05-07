470a112ba7d016e797bd26ce9dbc26da2b27ea4c
thrishna <thrishna.palissery@gmail.com>
2018-05-06 23:34:53 -0700

    User details view created - shows details of menu item selected

--- term project/User_details_ViewController.swift
+++ term project/User_details_ViewController.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class User_details_ViewController: UIViewController {
 
+    @IBOutlet weak var calories_label: UILabel!
+    @IBOutlet weak var price_label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name_label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if selected_section == 0
+        {
+            var name = drinks[selected_row].Name
+            imageView.image = UIImage(named: name)
+            name_label.text = name
+            price_label.text = String( drinks[selected_row].Unitprice )
+            calories_label.text = String(drinks[selected_row].Calories!)
+            category_label.text = "Drinks"
+        }
+        
+        if selected_section == 1
+        {
+            var name = appets[selected_row].Name
+            imageView.image = UIImage(named: name)
+            name_label.text = name
+            price_label.text =  String( appets[selected_row].Unitprice )
+            calories_label.text = String(appets[selected_row].Calories!)
+            category_label.text = "Appetizers"
+        }
+        if selected_section == 2
+        {
+            var name = mains[selected_row].Name
+            imageView.image = UIImage(named: name)
+            name_label.text = name
+            price_label.text = String( mains[selected_row].Unitprice )
+            calories_label.text = String(mains[selected_row].Calories!)
+            category_label.text = "Main Course"
+        }
+        if selected_section == 3
+        {
+            var name = deserts[selected_row].Name
+            imageView.image = UIImage(named: name)
+            name_label.text = name
+            price_label.text = String( deserts[selected_row].Unitprice )
+            calories_label.text = String(deserts[selected_row].Calories!)
+            category_label.text = "Deserts"
+        }
 
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var category_label: UILabel!
+    
    
     
 
