2b3dd3530778e5f547cc01d29ae454ad32780f74
shwetakothari <shweta.a.kothari@gmail.com>
2018-05-08 21:30:10 -0700

    date picker

---
+++ term project/Place_order_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Place_order_ViewController.swift
+//  term project
+//
+//  Created by Shweta Kothari on 5/8/18.
+//  Copyright © 2018 Shweta Kothari. All rights reserved.
+//
+
+import UIKit
+
+class Place_order_ViewController: UIViewController {
+
+    
+    @IBAction func date_picker(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
