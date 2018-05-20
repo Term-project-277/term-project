1430d41836b89a0c3a4474415ab0b4cd4e683c0e
thrishna <thrishna.palissery@gmail.com>
2018-05-19 23:46:27 -0700

    Added button for sorting on header view of status report

--- term project/Status_report_TableViewController.swift
+++ term project/Status_report_TableViewController.swift
@@ -48,21 +48,33 @@ class Status_report_TableViewController: UITableViewController {
         
         return report_orders.count
     }
+    
+    @objc func buttonClicked(sender:UIButton)
+    {
+        
+    }
   
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
         
         let frame = tableView.frame
-        let rect1 = CGRect(x: 300, y: 30, width: 300, height: 40)
-        let headerView = UIView(frame: rect1)
+        let rect = CGRect(x: 300, y: 30, width: 300, height: 40)
+        let headerView = UIView(frame: rect)
         headerView.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
-        let rect = CGRect(x: 0, y: 0, width: 300, height: 30)
-        let label = UILabel(frame: rect)
+        let rect1 = CGRect(x: 0, y: 0, width: 300, height: 30)
+        let label = UILabel(frame: rect1)
         label.text = "Status Report"
         //vw.backgroundColor = UIColor.red
         label.font = label.font.withSize(25)
         label.textColor = UIColor.black
+        let rect2 = CGRect(x: 200, y: 0, width: 300, height: 30)
+        let button = UIButton(frame: rect2)
+        button.tag = section
+        var image:UIImage!
+        button.setImage(UIImage(named: "connect-arrow-up-down-7.png"), for: UIControlState.normal)
+        button.addTarget(self,action:#selector(buttonClicked),for:.touchUpInside)
         headerView.addSubview(label)
+        headerView.addSubview(button)
         
         return headerView
     }
